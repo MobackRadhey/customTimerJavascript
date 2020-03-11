d7fe17ccdeffdfd18db3b6343a173edb75e15fb0
radhey <radheyr@moback.com>
2020-03-11 21:10:50 +0530

    updated functionality

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 
 var cTimer = (function(){
+    /**
+ * @function callTimeout
+ * @param {function} callback
+ * @param {number} milliseconds
+ * @returns {Object} worker
+ * @description takes a callback function and time in milliseconds, starts the timer using a 
+ * worker thread and returns a worker reference as an object 
+ */
     function callTimeout(cb,ms){
         if(window.Worker){
             let worker = new Worker('slave.js');
@@ -12,6 +20,13 @@ var cTimer = (function(){
             throw new Error("browser does not support worker api");
         }
     }
+    /**
+ * @function cancelTimeout
+ * @param {Object} worker
+ * @returns {null}
+ * @description takes a already created worker reference as input, cancels the timer 
+ * (terminates the worker thread) 
+ */
     function cancelTimeout(w_id){
         w_id.terminate();
         console.log(`cancelled cTimer`);
